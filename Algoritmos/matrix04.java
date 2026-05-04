671c128c06175511e73087546c325c665c48612a
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-05-04 19:45:30 -0300

    Update matrix04.java

--- Algoritmos/matrix04.java
+++ Algoritmos/matrix04.java
@@ -1,124 +1,88 @@
 import java.util.*;
 
 public class matrix04 {
+    public static void main(String [] arguments){
+        Scanner sc = new Scanner(System.in);
+        
+        char [][] tabuleiro = new char [3][3];
+        char jogadorAtual = 'X';
+        int linha, coluna;
+        boolean venceu = false;
+        int jogadas = 0;
 
-    static String[] board;
-    static String turn;
 
-    static String checkWinner() {
-        for (int a = 0; a < 8; a++) {
-            String line = null;
-
-            switch (a) {
-                case 0:
-                    line = board[0] + board[1] + board[2];
-                    break;
-                case 1:
-                    line = board[3] + board[4] + board[5];
-                    break;
-                case 2:
-                    line = board[6] + board[7] + board[8];
-                    break;
-                case 3:
-                    line = board[0] + board[3] + board[6];
-                    break;
-                case 4:
-                    line = board[1] + board[4] + board[7];
-                    break;
-                case 5:
-                    line = board[2] + board[5] + board[8];
-                    break;
-                case 6:
-                    line = board[0] + board[4] + board[8];
-                    break;
-                case 7:
-                    line = board[2] + board[4] + board[6];
-                    break;
-            }
-
-            if (line.equals("XXX")) { // Para o jogador X
-                return "X";
-            }
-
-            else if (line.equals("OOO")) { // Para o jogador O
-                return "O";
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro.length; j++) {
+                tabuleiro[i][j] = '-';
             }
         }
 
-        for (int a = 0; a < 9; a++) {
-            if (Arrays.asList(board).contains(String.valueOf(a + 1))) {
-                break;
-            } else if (a == 8) {
-                return "draw";
+        while(jogadas < 9 && !venceu) {
+            System.out.println("\nTabuleiro atual:");
+            for (int i = 0; i < tabuleiro.length; i++) {
+                for (int j = 0; j < tabuleiro.length; j++) {
+                    System.out.println(tabuleiro[i][j] + " ");
+                }
+                System.out.println();
             }
-        }
 
-        System.out.println("Jogador " + turn + " digite um número para colocar " + turn + ": ");
-        return null;
-    }
+            System.out.println("\nVez do Jogador " + jogadorAtual);
+            System.out.println("Informe a linha (0, 1, 2): ");
+            linha = sc.nextInt();
+            System.out.println("Informe a coluna (0, 1, 2): ");
+            coluna = sc.nextInt();
+            if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2) {
+                System.out.println("Posição inválida. Tente novamente.");
+            } else if (tabuleiro[linha][coluna] != '-') {
+                System.out.println("Essa posição já está preenchida. Tente novamente");
+            } else {
+                tabuleiro[linha][coluna] = jogadorAtual;
+                jogadas++;
 
-    static void printBoard() {
-        System.out.println("|---|---|---|");
-        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
-        System.out.println("|---|---|---|");
-    }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        board = new String[9];
-        turn = "X";
-        String winner = null;
-
-        for (int a = 0; a < 9; a++) {
-            board[a] = String.valueOf(a + 1);
-        }
-
-        System.out.println("Jogo da velha");
-        printBoard();
-        System.out.println("X vai jogar primeiro. Insira um número para colocar X:");
-
-        while (winner == null) {
-            int numInput;
-
-            try {
-                numInput = in.nextInt();
-
-                // Check range
-                if (!(numInput > 0 && numInput <= 9)) {
-                    System.out.println("Inválido! Insira outro número: ");
-                    continue;
+                // verifica linha
+                for (int i = 0; i < tabuleiro.length; i++) {
+                    if (tabuleiro[i][0] == jogadorAtual && tabuleiro[i][1] == jogadorAtual && tabuleiro[i][2] == jogadorAtual) {
+                        venceu = true;
+                    }
                 }
 
-                // Check if slot is available
-                if (board[numInput - 1].equals(String.valueOf(numInput))) {
-                    board[numInput - 1] = turn;
-
-                    // Toggle turn
-                    turn = turn.equals("X") ? "O" : "X";
-
-                    printBoard();
-                    winner = checkWinner();
-                } else {
-                    System.out.println("Já preenchido! Insira outro número: ");
+                // verifica coluna
+                for (int j = 0; j < tabuleiro.length; j++) {
+                    if (tabuleiro[0][j] == jogadorAtual && tabuleiro[1][j] == jogadorAtual && tabuleiro[2][j] == jogadorAtual) {
+                        venceu = true;
+                    }
                 }
 
-            } catch (InputMismatchException e) {
-                System.out.println("Inválido! Insira outro número: ");
-                in.nextLine(); // Consume invalid input to prevent infinite loop
+                // verifica diagonal principal
+                if(tabuleiro[0][0] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][2] == jogadorAtual) {
+                    venceu = true;
+                }
+
+                // verifica diagonal secundária
+                if(tabuleiro[0][2] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][0] == jogadorAtual) {
+                    venceu = true;
+                }
+
+                if(!venceu) {
+                    if(jogadorAtual == 'X') {
+                        jogadorAtual = 'O';
+                    } else {
+                        jogadorAtual = 'X';
+                    }
+                }
             }
         }
-
-        // Final result
-        if (winner.equalsIgnoreCase("empate")) {
-            System.out.println("Empate! Obrigado por jogar");
+        System.out.println("Tabuleiro final: ");
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                System.out.println(tabuleiro[i][j] + " ");
+            }
+        } if (venceu) {
+            System.out.println("Parabéns! Você venceu!");
         } else {
-            System.out.println("Parabéns! " + winner + " , você venceu! Obrigado por jogar!");
+            System.out.println("Deu velha!");
         }
 
-        in.close();
+        sc.close();
     }
 }
